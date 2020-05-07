703979d4293aae1048a38b45efa2ddb117a5c2d0
kimgyuhee <42735333+kimgyuhee@users.noreply.github.com>
2020-05-07 17:09:34 +0900

    수학 1

---
+++ 수학 1/Hw_1712.java
@@ -0,0 +1,17 @@
+package beakjoon9;
+
+import java.util.Scanner;
+public class Hw_1712 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int A=sc.nextInt();
+		int B=sc.nextInt();
+		int C=sc.nextInt();
+		
+		if(B>=C)
+			System.out.println(-1);
+		else
+			System.out.println(A/(C-B)+1);
+	}
+
+}
---
+++ 수학 1/Hw_2839.java
@@ -0,0 +1,10 @@
+package beakjoon9;
+
+public class Hw_2839 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
