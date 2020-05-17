5a4c81789db669496bf26825e15365b757c60a6f
kimgyuhee <42735333+kimgyuhee@users.noreply.github.com>
2020-05-17 15:08:55 +0900

    Update Hw_1193.java

--- 수학 1/Hw_1193.java
+++ 수학 1/Hw_1193.java
@@ -1,5 +1,30 @@
 package beakjoon9;
 
-public class Hw_1193 {
+import java.util.Scanner;
 
+public class Hw_1193 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int X = sc.nextInt();
+		int n=0;
+		int i=0;
+		int x=0, y=0;
+		while(X>n) {
+			if(X<=n+i)
+				break;
+			n=n+i;
+			i++;
+		}
+		
+		if(i%2!=0) {
+			x=i-(X-n-1);
+			y=X-n;
+		}else {
+			y=i-(X-n-1);
+			x=X-n;
+		}
+		System.out.println(x+"/"+y);
+		
+	}
 }
